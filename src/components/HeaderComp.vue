9591b31c197e14a911b8ea2569820d2ca1052f07
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-05-27 13:20:41 +0200

    lavorando sul Header

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>H</div>
+  <header class="container-fluid">
+    <div class="top container d-flex">
+      <div class="left d-flex">
+        <a>ORDER ONLINE</a>
+
+        <nav>
+          <ul class="d-flex">
+            <li>HOME</li>
+            <li>PAGES</li>
+            <li>MENU</li>
+          </ul>
+        </nav>
+
+      </div>
+
+      <div class="center">
+        <img src="../ass" alt="">
+      </div>
+
+      <div class="right">
+
+      </div>
+    </div>
+
+    <div class="bottom"></div>
+  </header>
 </template>
 
 <script>
@@ -9,5 +34,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+header{
+  height: 700px;
+  background-image: url("../assets/img/header-background.jpg");
+  .top{
+    height: 150px;
+  }
+}
 
 </style>
