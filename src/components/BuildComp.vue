f49522dcb55c4c393969983229f4ba853c06cc9a
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-05-29 18:33:22 +0200

    biuld component

--- src/components/BuildComp.vue
+++ src/components/BuildComp.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="container-fluid rl-container">
-    
+    <div class="text">
+      <h1>
+        GO AHEAD AND BUILD YOUR OWN PIZZA WE WON’T JUDGE!
+      </h1>
+    </div>
+
   </div>
 </template>
 
@@ -12,11 +17,20 @@ export default {
 
 <style lang="scss" scoped>
 .rl-container{
+  position: relative;
   height: 500px;
   background-image: url("../assets/img/h3-background-img.jpg");
   background-size: cover;
   background-position: center;
   background-position: 50% 50%;
+  .text{
+    position: absolute;
+    width: 20%;
+    font-weight: bold;
+    color: white;
+    top: 120px;
+    left: 150px;
+  }
 }
 
 </style>
